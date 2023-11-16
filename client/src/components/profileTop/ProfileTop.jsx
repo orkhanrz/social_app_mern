81d7ff80bee74ccc7e2349efdff202b7feeb12a3
Orkhan Rzali <orkhan.rz8@gmail.com>
2023-11-16 13:09:42 +0400

    photos initialized

--- client/src/components/profileTop/ProfileTop.jsx
+++ client/src/components/profileTop/ProfileTop.jsx
@@ -55,111 +55,117 @@ export default function ProfileTop() {
           <img src={config.backend_url + user.profilePicture} alt="user" />
         </div>
       </div>
-      <div className="profileContainer">
-        <div className="profileInfoTop">
-          <div className="profileInfoTopLeft">
-            <div className="profileInfoTopLeftImagePlaceholder"></div>
-            <div className="profileInfoTopLeftUsername">
-              <h1>{user.firstName + " " + user.lastName}</h1>
+      <div className="profileInfo">
+        <div className="profileInfoContainer">
+          <div className="profileInfoTop">
+            <div className="profileInfoTopLeft">
+              <div className="profileInfoTopLeftImagePlaceholder"></div>
+              <div className="profileInfoTopLeftUsername">
+                <h1>{user.firstName + " " + user.lastName}</h1>
+              </div>
             </div>
-          </div>
-          {me.username !== user.username ? (
-            <div className="profileInfoTopRight">
-              <button
-                className="profileInfoTopRightBtn blue"
-                onClick={handleFollow}
-              >
-                <>
+            {me.username !== user.username ? (
+              <div className="profileInfoTopRight">
+                <button
+                  className="profileInfoTopRightBtn blue"
+                  onClick={handleFollow}
+                >
+                  <>
+                    <span className="profileInfoTopRightBtnIcon">
+                      <SubscriptionsOutlined />
+                    </span>
+                    <span className="profileInfoTopRightBtnText">
+                      {isFollowed ? "Unfollow" : "Follow"}
+                    </span>
+                  </>
+                </button>
+                <button className="profileInfoTopRightBtn">
                   <span className="profileInfoTopRightBtnIcon">
-                    <SubscriptionsOutlined />
+                    <MessageOutlined />
+                  </span>
+                  <span className="profileInfoTopRightBtnText">Message</span>
+                </button>
+                <div className="profileInfoTopRightBtn">
+                  <span className="profileInfoTopRightBtnIcon">
+                    <ExpandMoreOutlined />
+                  </span>
+                </div>
+              </div>
+            ) : (
+              <div className="profileInfoTopRight">
+                <button className="profileInfoTopRightBtn blue">
+                  <span className="profileInfoTopRightBtnIcon">
+                    <Add />
                   </span>
                   <span className="profileInfoTopRightBtnText">
-                    {isFollowed ? "Unfollow" : "Follow"}
+                    Add to story
                   </span>
-                </>
-              </button>
-              <button className="profileInfoTopRightBtn">
-                <span className="profileInfoTopRightBtnIcon">
-                  <MessageOutlined />
-                </span>
-                <span className="profileInfoTopRightBtnText">Message</span>
-              </button>
-              <div className="profileInfoTopRightBtn">
-                <span className="profileInfoTopRightBtnIcon">
-                  <ExpandMoreOutlined />
-                </span>
+                </button>
+                <button
+                  className="profileInfoTopRightBtn"
+                  onClick={toggleEditProfile}
+                >
+                  <span className="profileInfoTopRightBtnIcon">
+                    <Edit />
+                  </span>
+                  <span className="profileInfoTopRightBtnText">
+                    Edit profile
+                  </span>
+                </button>
+                <div className="profileInfoTopRightBtn">
+                  <span className="profileInfoTopRightBtnIcon">
+                    <ExpandMoreOutlined />
+                  </span>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="profileInfoTopRight">
-              <button className="profileInfoTopRightBtn blue">
-                <span className="profileInfoTopRightBtnIcon">
-                  <Add />
-                </span>
-                <span className="profileInfoTopRightBtnText">Add to story</span>
-              </button>
-              <button
-                className="profileInfoTopRightBtn"
-                onClick={toggleEditProfile}
-              >
-                <span className="profileInfoTopRightBtnIcon">
-                  <Edit />
-                </span>
-                <span className="profileInfoTopRightBtnText">Edit profile</span>
-              </button>
-              <div className="profileInfoTopRightBtn">
-                <span className="profileInfoTopRightBtnIcon">
-                  <ExpandMoreOutlined />
-                </span>
-              </div>
-            </div>
-          )}
-        </div>
-        <hr />
-        <div className="profileInfoBottom">
-          <div className="profileInfoBottomLeft">
-            <Link
-              to={`/${username}`}
-              className={`profileInfoBottomLeftItem ${
-                pathname === `/${username}` ? "active" : ""
-              }`}
-            >
-              Posts
-            </Link>
-            <span
-              className={`profileInfoBottomLeftItem ${
-                pathname === `/${username}/about` ? "active" : ""
-              }`}
-            >
-              About
-            </span>
-            <span
-              className={`profileInfoBottomLeftItem ${
-                pathname === `/${username}/friends` ? "active" : ""
-              }`}
-            >
-              Friends
-            </span>
-            <Link
-              to={`/${username}/photos`}
-              className={`profileInfoBottomLeftItem ${
-                pathname === `/${username}/photos` ? "active" : ""
-              }`}
-            >
-              Photos
-            </Link>
-            <span
-              className={`profileInfoBottomLeftItem ${
-                pathname === `/${username}/videos` ? "active" : ""
-              }`}
-            >
-              Videos
-            </span>
+            )}
           </div>
-          <div className="profileInfoBottomRight">
-            <button className="profileInfoBottomRightBtn">
-              <MoreHorizOutlined />
-            </button>
+          <hr />
+          <div className="profileInfoBottom">
+            <div className="profileLinks">
+              <Link
+                to={`/${username}`}
+                className={`profileLink ${
+                  pathname === `/${username}` ? "active" : ""
+                }`}
+              >
+                Posts
+              </Link>
+              <span
+                className={`profileLink ${
+                  pathname === `/${username}/about` ? "active" : ""
+                }`}
+              >
+                About
+              </span>
+              <span
+                className={`profileLink ${
+                  pathname === `/${username}/friends` ? "active" : ""
+                }`}
+              >
+                Friends
+              </span>
+              <Link
+                to={`/${username}/photos`}
+                className={`profileLink ${
+                  pathname === `/${username}/photos` ? "active" : ""
+                }`}
+              >
+                Photos
+              </Link>
+              <span
+                className={`profileLink ${
+                  pathname === `/${username}/videos` ? "active" : ""
+                }`}
+              >
+                Videos
+              </span>
+            </div>
+            <div className="profileInfoBottomRight">
+              <button className="profileInfoBottomRightBtn">
+                <MoreHorizOutlined />
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- client/src/pages/photos/Photos.jsx
+++ client/src/pages/photos/Photos.jsx
@@ -2,10 +2,13 @@ import "./photos.css";
 
 import Topbar from "../../components/topbar/Topbar";
 import ProfileTop from "../../components/profileTop/ProfileTop";
-// import { useLoaderData } from "react-router-dom";
+import ProfileCard from "../../components/profileCard/ProfileCard";
+import { useLoaderData, useLocation, useParams, Link } from "react-router-dom";
 
 export default function Photos() {
-  // const { results: user } = useLoaderData();
+  const { username } = useParams();
+  const { pathname } = useLocation();
+  const { results: user } = useLoaderData();
 
   return (
     <>
@@ -13,7 +16,43 @@ export default function Photos() {
       <ProfileTop />
       <div className="photosPage">
         <div className="profileContainer">
-          <h1>Photos</h1>
+          <ProfileCard title="Photos">
+            <div className="profileLinks">
+              <Link
+                to={`/${username}/photos`}
+                className={`profileLink ${
+                  pathname === `/${username}/photos` ? "active" : ""
+                }`}
+              >
+                {user.firstName}'s photos
+              </Link>
+              <Link
+                to={`/${username}/albums`}
+                className={`profileLink ${
+                  pathname === `/${username}/albums` ? "active" : ""
+                }`}
+              >
+                Albums
+              </Link>
+            </div>
+            <div className="photosItems">
+              <div className="photosItem">
+                <img className="photosItemImg" src="/assets/images/image1.jpg" alt="" />
+              </div>
+              <div className="photosItem">
+                <img className="photosItemImg" src="/assets/images/image1.jpg" alt="" />
+              </div>
+              <div className="photosItem">
+                <img className="photosItemImg" src="/assets/images/image1.jpg" alt="" />
+              </div>
+              <div className="photosItem">
+                <img className="photosItemImg" src="/assets/images/image1.jpg" alt="" />
+              </div>
+              <div className="photosItem">
+                <img className="photosItemImg" src="/assets/images/image1.jpg" alt="" />
+              </div>
+            </div>
+          </ProfileCard>
         </div>
       </div>
     </>
